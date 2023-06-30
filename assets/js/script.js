b16101294cbe2ded6017916fa36ad2585e22efb7
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-06-29 20:21:48 -0500

    update scrip, questions appear in random order

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,16 @@
 let correct = 0;
 let wrong = 0;
-let score = (correct/5)*100 + "%";
+let score = (correct/6)*100 + "%";
 let intials = "";
-let question_set = {
-    question: "Which is a variable prefix in Javascript",
-    a: "A. var",
-    b: "B. who",
-    c: "C. cont",
-    d: "D. text",
-    answer: "a"
-};
-let question_set1 = {    
-    question: "How deos a WHILE Loop begin",
+let question_set = {    
+    question: "How does a WHILE Loop begin",
     a: "while i = 1 to 10",
     b: "while (i <= 10; i++)",
     c: "while (i <= 10)",
     d: "whie {i<= 10; i++}",
     answer: "c"
 };
-let question_set2 = {
+let question_set1 = {
     question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
     a: "if i =! 5 then",
     b: "if (i != 5)",
@@ -26,7 +18,7 @@ let question_set2 = {
     d: "if i <> 5",
     answer: "b",
 };
-let question_set3 = {
+let question_set2 = {
     question: "How to write an IF statement in JavaScript?",
     a: "if i = 5 then",
     b: "if i = 5",
@@ -34,7 +26,7 @@ let question_set3 = {
     d: "if (i == 5)",
     answer: "d",
 };
-let question_set4 = {
+let question_set3 = {
     question: "How do you call a function named 'myFunction'?",
     a: "call function myFunction()",
     b: "call myFunction()",
@@ -42,7 +34,7 @@ let question_set4 = {
     d: "myFunction",
     answer: "c",
 };
-let question_set5 = {
+let question_set4 = {
     question: "How do you create a function in JavaScript?",
     a: "function = myFunction()",
     b: "function myFunction()",
@@ -50,17 +42,13 @@ let question_set5 = {
     d: "def myFunction()",
     answer: "b",
 };
+let question_array=[question_set,question_set1,question_set2,question_set3,question_set4];
+
 
 let time = 60;
 let timing;
 
-// Variables for test_card
-let q_display = document.getElementById("questions").textContent = question_set.question;
-let answer_a = document.getElementById("a").textContent = question_set.a;
-let answer_b = document.getElementById("b").textContent = question_set.b;
-let answer_c = document.getElementById("c").textContent = question_set.c;
-let answer_d = document.getElementById("d").textContent = question_set.d;
-let right_or_wrong = document.getElementById("right_or_wrong").textContent= "";
+
 
 // Variable for score_card
 let first_place = document.getElementById("1st").textContent = "gg"
@@ -72,6 +60,18 @@ function start(){
         start_quiz.style.display = "block";
     let hide_score = document.querySelector('.score_card')
         hide_score.style.display = "none";
+
+
+        // Variables for test_card
+    let question_array=[question_set,question_set1,question_set2,question_set3,question_set4];
+    question_array.sort(()=>0.5 - Math.random());
+    let q_display = document.getElementById("questions").textContent = question_array[0].question;
+    let answer_a = document.getElementById("a").textContent = question_array[0].a;
+    let answer_b = document.getElementById("b").textContent = question_array[0].b;
+    let answer_c = document.getElementById("c").textContent = question_array[0].c;
+    let answer_d = document.getElementById("d").textContent = question_array[0].d;
+    let right_or_wrong = document.getElementById("right_or_wrong").textContent= "";
+        
 
     function count_down(){
         time = 60;  
