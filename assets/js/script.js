427d2b0a40087cec2d998e96c8eb1fda9cb68025
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-07-04 15:05:14 -0500

    add display_hide_card function to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
+// Sets global variables
 let time = 60;
 let timing;
-let correct = 0
+let correct = 0;
 let answered_qs= 0;
 let score = 0;
 let initial = "";
+let q_display ="";
 let answer_a = "";
 let answer_b = "";
 let answer_c = "";
 let answer_d = "";
+// Object for scoreboard
 let score_board = [
     {
       name: "GJS",
@@ -22,7 +25,7 @@ let score_board = [
       score: 40
     }
   ];
-  
+// Objects holding questions
 let question_set = {    
     question: "How does a WHILE Loop begin",
     a: "while i = 1 to 10",
@@ -63,103 +66,90 @@ let question_set4 = {
     d: "def myFunction()",
     answer: "b",
 };
+// Array hold question objects
 let question_array=[question_set,question_set1,question_set2,question_set3,question_set4];
+// Global variables for score board display
+let first_place = document.getElementById("1st").textContent = `Name:${score_board[0].name}  Score:${score_board[0].score}`;
+let second_place = document.getElementById("2nd").textContent = `Name:${score_board[1].name}  Score:${score_board[1].score}`;
+let third_place = document.getElementById("3rd").textContent = `Name:${score_board[2].name}  Score:${score_board[2].score}`;
+// function that displays and hides cards
+function display_hide_card(show,hide_1,hide_2){
+    document.querySelector(show).style.display = "block";
+    document.querySelector(hide_1).style.display = "none";
+    document.querySelector(hide_2).style.display = "none";
+};
 
-// Variable for score_card
-let first_place = document.getElementById("1st").textContent = `Name:${score_board[0].name}  Score:${score_board[0].score}`
-let second_place = document.getElementById("2nd").textContent = `Name:${score_board[1].name}  Score:${score_board[1].score}`
-let third_place = document.getElementById("3rd").textContent = `Name:${score_board[2].name}  Score:${score_board[2].score}`
-
+// Function to begin quiz
 function start(){
     document.getElementById("start").textContent = "RESTART";
-    let start_quiz = document.querySelector(".test_card");
-        start_quiz.style.display = "block";
-    let hide_score = document.querySelector('.score_card')
-        hide_score.style.display = "none";
-    let name_entry_show = document.querySelector('.name_card')
-        name_entry_show.style.display = "none";
-
+    display_hide_card('.test_card', '.score_card', '.name_card');
         // Variables for test_card
         question_array.sort(()=>0.5 - Math.random());
         answered_qs= 0;
-        quiz()
-
+        quiz();
+    // Function for timer
     function count_down(){
         time = 60;  
         counter = document.getElementById("counter");
-        clearInterval(timing)
+        clearInterval(timing);
         timing = setInterval(() => {
             
             counter.textContent = time;
             time--;
             if(time < 0){
                 clearInterval(timing);
-                alert(`TIMES UP!\nYour Score: ${score}` )
+                alert(`TIMES UP!\nYour Score: ${score}` );
                 // show_scores()
-                show_name_entry()
+                show_name_entry();
             }
         },1000);
     } 
     count_down();     
 };
-
+//  Function to make scoreboard visble and quiz_card and name_card hidden
 function show_scores(){
     document.getElementById("start").textContent = "START";
-    let score_stats = document.querySelector(".score_card");
-        score_stats.style.display = "block";
-    time = 60;
-    let hide_start = document.querySelector('.test_card')
-        hide_start.style.display = "none";
-    let name_entry_show = document.querySelector('.name_card')
-        name_entry_show.style.display = "none";
+    display_hide_card('.score_card', '.test_card', '.name_card');
+    time = 60;  
     counter = document.getElementById("counter");
     counter.textContent = time;
     clearInterval(timing); 
 };
-
+// Functionn for displaying name_card and hiding others
 function show_name_entry(){
-    let hide_start = document.querySelector(".test_card");
-        hide_start.style.display = "none";
-    let hide_score = document.querySelector('.score_card')
-        hide_score.style.display = "none";
-    let name_entry_show = document.querySelector('.name_card')
-        name_entry_show.style.display = "block";
+    display_hide_card( '.name_card', '.score_card', '.test_card');
     counter = document.getElementById("counter");
     counter.textContent = time;
-    clearInterval(timing)
+    clearInterval(timing);
 
 };
-
+//  Function diaplying score_board and for submitting name and score into score board
 function submit_name_score(){
-    let hide_score = document.querySelector('.score_card')
-        hide_score.style.display = "block";
-    let name_entry_show = document.querySelector('.name_card')
-        name_entry_show.style.display = "none";
+    display_hide_card('.score_card', '.test_card', '.name_card');
     let initial = document.getElementById("name_entry").value.toUpperCase();
     if(initial.length != 3){
         alert("Enter Only 3 Letters");
-        show_name_entry()
+        show_name_entry();
     } else{
-        sort_board(initial)
+        sort_board(initial);
     };   
 };
-
+// Function for inserting name and score into scoreboard
 function sort_board(name_input){
-    // let initial = document.getElementById("name_entry").value;
     score_board.sort((a, b) => b.score - a.score); 
     // IF statement that adds the name and score IF is greater than last of the score_board index
     if(score>score_board[score_board.length- 1].score){
         score_board.pop();
-        score_board.push({name: name_input, score: score})
+        score_board.push({name: name_input, score: score});
         score_board.sort((a, b) => b.score - a.score);
     }
-    first_place = document.getElementById("1st").textContent = `Name:${score_board[0].name}  Score:${score_board[0].score}`
-    second_place = document.getElementById("2nd").textContent = `Name:${score_board[1].name}  Score:${score_board[1].score}`
-    third_place = document.getElementById("3rd").textContent = `Name:${score_board[2].name}  Score:${score_board[2].score}`
+    first_place = document.getElementById("1st").textContent = `Name:${score_board[0].name}  Score:${score_board[0].score}`;
+    second_place = document.getElementById("2nd").textContent = `Name:${score_board[1].name}  Score:${score_board[1].score}`;
+    third_place = document.getElementById("3rd").textContent = `Name:${score_board[2].name}  Score:${score_board[2].score}`;
 
         return score_board
 }
-
+// Function that handles question answering and displaying new questions
 function quiz(answer){
     if(answer == question_array[answered_qs].answer){
         right_or_wrong.textContent= "Correct";
@@ -183,7 +173,7 @@ function quiz(answer){
         right_or_wrong = document.getElementById("right_or_wrong");
     };
 };
-    
+// Function that will update variables correct & score for quiz function   
 function update_correct(update){
     correct = update;
     score = (correct/5)*100;
